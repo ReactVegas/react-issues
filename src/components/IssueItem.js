6ff26a788af2bb322eac4b5345834d9ccec34a37
Fred Guest <fredguest@mac.com>
2015-09-10 09:13:14 -0700

    add spring animation to display issue onClick

--- src/components/IssueItem.js
+++ src/components/IssueItem.js
@@ -1,4 +1,5 @@
 import {PropTypes, Component} from 'react';
+import {Spring} from 'react-motion';
 import marked from 'marked';
 
 class IssueItem extends Component {
@@ -23,17 +24,30 @@ class IssueItem extends Component {
   }
 
   renderMore() {
-    if (this.state.showMore) {
-      let html = marked(this.props.issue.body);
+    let html = marked(this.props.issue.body);
 
-      return (
-        <div dangerouslySetInnerHTML={{__html: html}} />
-      );
-    }
+    return (
+      <Spring defaultValue={{height: {val: 0}, opacity: {val: 0}}} endValue={this.endValue()}>
+        {(animated) =>
+          <div
+            style={{height: animated.height.val, opacity: animated.opacity.val, overflow: 'auto'}}
+            dangerouslySetInnerHTML={{__html: html}}
+          />
+        }
+      </Spring>
+    );
   }
 
   handleClick() {
     this.setState({showMore: !this.state.showMore});
+  }
+
+  endValue() {
+    const {showMore} = this.state;
+    return {
+      height: {val: showMore ? 100 : 0},
+      opacity: {val: showMore ? 1 : 0}
+    };
   }
 }
 
