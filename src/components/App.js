cd608dd0d6c9002769e5784794cc9210810a0fb3
Andrew McCloud <andrew.mccloud@formsdirect.net>
2015-12-16 19:59:08 -0800

    Add ability to pick repo

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,34 @@
-import {Component} from 'react';
+import {Component, findDOMNode} from 'react';
 import IssueList from './IssueList';
 
 class App extends Component {
+  state = {
+    repo: 'facebook/react'
+  }
+
   render() {
     return (
       <div>
-        <h1>React Issues</h1>
-        <IssueList repo="facebook/react" />
+        <h1>Issues ({this.state.repo})</h1>
+        <form onSubmit={::this.handleLoad}>
+          <input ref="repoInput" />
+          <button type="submit">Load</button>
+        </form>
+        <IssueList repo={this.state.repo} />
       </div>
     );
+  }
+
+  handleLoad(event) {
+    event.preventDefault();
+
+    let input = findDOMNode(this.refs.repoInput);
+
+    this.setState({
+      repo: input.value
+    });
+
+    input.value = '';
   }
 }
 
